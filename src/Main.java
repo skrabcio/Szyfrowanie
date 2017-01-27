dae0e4853e0342332f6e4c4edf420fde29e22add
skrab <skrab@DESKTOP-25V4D6G>
2017-01-27 18:21:07 +0100

    2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,45 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class Main {
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends JFrame implements ActionListener {
 
+	private JButton wyb_plik;
+	public Main() {
+		
+		setSize(300,200);
+		setTitle("Szyfracja");
+		setLayout(null);
+		
+		wyb_plik= new JButton("Wybierz Plik");
+		wyb_plik.setBounds(50,100,120,25);
+		add(wyb_plik);
+		wyb_plik.addActionListener(this);
+		
 	}
+	public static void main(String[] args) {
 
+		Main aplikacja = new Main();
+		aplikacja.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		aplikacja.setVisible(true);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		Object z = e.getSource();
+		
+		if(z==wyb_plik){
+			JFileChooser fc = new JFileChooser();
+			if(fc.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+				File plik = fc.getSelectedFile();
+				JOptionPane.showMessageDialog(null,"Wybrany plik:" + fc.getName());
+			}
+		}
+		
+	}
 }
