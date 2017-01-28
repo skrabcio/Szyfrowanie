8c2e240582397f31579278de3ddc29ffca76ad4b
skrab <skrab@DESKTOP-25V4D6G>
2017-01-28 16:23:55 +0100

    4

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,41 @@ import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Main extends JFrame implements ActionListener {
 
-	private JButton wyb_plik;
+	private JButton wyb_plik, gen_pass;
+	private JLabel lp;
+	private JTextField tp;
+	
 	public Main() {
 		
-		setSize(300,200);
+		setSize(300,400);
 		setTitle("Szyfracja");
 		setLayout(null);
 		
 		wyb_plik= new JButton("Wybierz Plik");
-		wyb_plik.setBounds(50,100,120,25);
+		wyb_plik.setBounds(90,25,120,25);
 		add(wyb_plik);
 		wyb_plik.addActionListener(this);
 		
+		lp = new JLabel("Podaj has³o:");
+		lp.setBounds(25,50,120,25);
+		add (lp);
+		
+		
+		tp = new JTextField("");
+		tp.setBounds(25,75,150,25);
+		add(tp);
+		tp.addActionListener(this);
+
+		gen_pass= new JButton("Generuj");
+		gen_pass.setBounds(175,75,100,25);
+		add(gen_pass);
+		gen_pass.addActionListener(this);
 	}
 	public static void main(String[] args) {
 
@@ -36,22 +55,21 @@ public class Main extends JFrame implements ActionListener {
 		Scanner wtekst = new Scanner(plik);
 		String tekst1 = wtekst.nextLine();
 		
-		byte[] tabtekst = tekst1.getBytes();
-		StringBuilder tekstb = new StringBuilder();
-		  for (byte b : tabtekst){
+		byte[] tab_tekst = tekst1.getBytes();
+		StringBuilder tekst_bity = new StringBuilder();
+		  for (byte b : tab_tekst){
 		     int war = b;
 		     for (int i = 0; i < 8; i++){
-		        tekstb.append((war & 128) == 0 ? 0 : 1);
+		        tekst_bity.append((war & 128) == 0 ? 0 : 1);
 		        war <<= 1;
 		     }
-		     tekstb.append(' ');
+		     tekst_bity.append(' ');
 		  }
-		 char[] tabtekstb = new char[tekstb.length()];
-		 	for( int xt = 0; xt < tekstb.length(); xt++){
-				tabtekstb[xt] = tekstb.charAt(xt);	
-				
+		 char[] tab_tekst_bity = new char[tekst_bity.length()];
+		 	for( int xt = 0; xt < tekst_bity.length(); xt++){
+				tab_tekst_bity[xt] = tekst_bity.charAt(xt);		
 		 	}
-		 	System.out.print(tabtekstb);
+		 	System.out.print(tab_tekst_bity);
 
 	}
 	@Override
