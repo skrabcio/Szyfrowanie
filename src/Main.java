a29affcc14720d1216d54c6f32a4f27f0a4cac01
skrab <skrab@DESKTOP-25V4D6G>
2017-01-28 17:18:30 +0100

    ver 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -11,10 +12,13 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class Main extends JFrame implements ActionListener {
-
-	private JButton wyb_plik, gen_pass;
+	private String tekst1;
+	private JButton wyb_plik, gen_pass, butt_szyfr;
 	private JLabel lp;
 	private JTextField tp;
+	private char[] tab_tekst_bity;
+	
+
 	
 	public Main() {
 		
@@ -41,6 +45,11 @@ public class Main extends JFrame implements ActionListener {
 		gen_pass.setBounds(175,75,100,25);
 		add(gen_pass);
 		gen_pass.addActionListener(this);
+		
+		butt_szyfr= new JButton("Szyfruj");
+		butt_szyfr.setBounds(25,250,100,25);
+		add(butt_szyfr);
+		butt_szyfr.addActionListener(this);
 	}
 	public static void main(String[] args) {
 
@@ -50,13 +59,32 @@ public class Main extends JFrame implements ActionListener {
 		
 	}
 	
-	public void approvedSelection(String plik){
+	public void szyfrowanie(){
+		byte[] tab_tekst_zak = new byte[tekst1.length()];
+		for(int k=0; k<licznik; k++){
+			if(tab_tekst_bity[k]== tabszyfrb[k]){
+				if(tab_tekst_bity[k]==' '){
+					tab_tekst_zak[k]=' ';
+				}
+				else{
+					tab_tekst_zak[k]='0';}
+			}
+			else{
+				tab_tekst_zak[k]='1';
+			}
+		}
+}
+	
+	public void approvedSelection(File tekst) throws FileNotFoundException{
 		
-		Scanner wtekst = new Scanner(plik);
-		String tekst1 = wtekst.nextLine();
+		
+		Scanner wtekst = new Scanner(tekst);
+		tekst1 = wtekst.nextLine();
 		
 		byte[] tab_tekst = tekst1.getBytes();
 		StringBuilder tekst_bity = new StringBuilder();
+		
+		
 		  for (byte b : tab_tekst){
 		     int war = b;
 		     for (int i = 0; i < 8; i++){
@@ -65,8 +93,8 @@ public class Main extends JFrame implements ActionListener {
 		     }
 		     tekst_bity.append(' ');
 		  }
-		 char[] tab_tekst_bity = new char[tekst_bity.length()];
-		 	for( int xt = 0; xt < tekst_bity.length(); xt++){
+		  char[] tab_tekst_bity = new char[tekst_bity.length()];
+		 	for( int xt = 0; xt < tekst_bity.length()-1; xt++){
 				tab_tekst_bity[xt] = tekst_bity.charAt(xt);		
 		 	}
 		 	System.out.print(tab_tekst_bity);
@@ -82,10 +110,18 @@ public class Main extends JFrame implements ActionListener {
 			if(fc.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
 				String plik = fc.getSelectedFile().getAbsolutePath();
 				JOptionPane.showMessageDialog(null,"Wybrany plik:" + fc.getSelectedFile().getAbsolutePath());
-			
-				approvedSelection(plik);
+				File tekst = new File(plik);
+				try {
+					approvedSelection(tekst);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 		
 			}
+			
+		}
+		else if(z == tp || z == butt_szyfr){
 			
 		}
 		
